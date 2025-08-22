14934d33753b931fb0166788ce67de367e8cc6ab
Md Atikur Rahman <mdatikur.contact@gmail.com>
2025-08-22 09:17:10 +0600

    add auto meta

--- CommentDetection.cpp
+++ CommentDetection.cpp
@@ -1,37 +1,26 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<regex>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
 using namespace std;
 
-int main()
-{
-    fstream file("test.txt");
-
+int main() {
+    ifstream file("test.txt");
 
     regex comment(R"((\/\/[^\n]*)|(\/\*[\s\S]*?\*\/))");
+    string code((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
 
-    string code, line;
-    while(getline(file,line))
-    {
-        code += line + "\n";
-    }
-
-    // Search for comments in the whole file
     auto begin = sregex_iterator(code.begin(), code.end(), comment);
     auto end = sregex_iterator();
 
-    if(begin == end) {
+    if (begin == end) {
         cout << "No comments found." << endl;
     } else {
         cout << "Comments detected:" << endl;
-        for(auto i = begin; i != end; ++i)
-        {
-            smatch match = *i;
-            cout << match.str() << endl;  
+        for (auto i = begin; i != end; ++i) {
+            cout << i->str() << endl;
         }
     }
 
-    file.close();
     return 0;
 }
